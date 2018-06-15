00814225fe3ad6e06a145aaf79e06cf928f25345
Omniprezz <geoffrey.frioli@gmail.com>
2018-06-15 16:27:52 +0200

    init salles clients

---
+++ init.sql
@@ -0,0 +1,21 @@
+INSERT INTO salles (ID_salle, capacite, equipement)
+    VALUES  (1, 120, 'Dolby'),
+            (2, 280, '4DX'),
+            (3, 178, 'THX'),
+            (4, 370, "3D"),
+            (5, 400, 'IMAX'),
+            (6, 220, "3D");
+
+INSERT INTO clients (nom, prenom, age, mail, ville)
+    VALUES  ('pepone', 'marcel', 68, 'pepone.marcel@gfail.com', 'saint-gaudens'),
+            ('durand', 'micheline', 73, 'vazymicheline@aol.fr', 'valentine'),
+            ('lebelge', 'jérémy', 24, 'lebelge@hehe.fr', 'montréjeau'),
+            ('coquille', 'gilbert', 42, 'coquillou@mail.com', 'villeneuve'),
+            ('durand', 'nicolas', 48, 'durand.nicolas@gfail.com', 'saint-gaudens'),
+            ('dupont', 'jean', 53, 'jeandup@aol.fr', 'miramont'),
+            ('morgan', 'clara', 37, 'clara@hotx.com', 'soueich'),
+            ('petite', 'jeune', 19, 'mapetite@jeune.fr', 'soueich'),
+            ('lupin', 'arsene', 65, 'arsene-lupin@vol.io', 'labarthe'),
+            ('michel', 'celine', 28, 'celine.michel@gfail.com', 'saint-gaudens'),
+            ('michel', 'jacquie', 52, 'jacquie&michel@hotx.com', 'lannemezan');
+
